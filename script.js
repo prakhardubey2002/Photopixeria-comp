9ca2c756675d184f88099908e79f7a580e4ff2ef
prakhardubey2002 <80157067+prakhardubey2002@users.noreply.github.com>
2021-08-31 11:18:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.portfolio-item').isotope({
+    // options
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+  });
+  $('.portfolio-menu ul li').click(function(){
+  $('.portfolio-menu ul li').removeClass('active');
+  $(this).addClass('active');
+  
+
+  var selector = $(this).attr('data-filter');
+  $('.portfolio-item').isotope({
+    filter:selector
+  });
+  return false;
+  });
+  
